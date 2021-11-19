1b1a0a35d4486d4d1936b99af250bed50baaa34a
agatadrozdowska <agata.drozdowska@icloud.com>
2021-11-19 15:49:41 -0600

    Implement dynamic rendering

--- client/src/components/Level1.jsx
+++ client/src/components/Level1.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Input, Button, Progress, Divider } from 'antd';
+import { AudioFilled } from '@ant-design/icons'
 import { level1 } from '../data.js';
+import { Link } from 'react-router-dom';
+import Level2 from './Level2.jsx';
 
 class Level1 extends React.Component {
 
@@ -21,6 +24,8 @@ class Level1 extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.checkMatched = this.checkMatched.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
+    this.handleAudio = this.handleAudio.bind(this);
   }
 
   componentDidMount() {
@@ -66,7 +71,6 @@ class Level1 extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log('hello')
 
     if (this.state.timeOut) {
       return alert('Please click restart button to keep going');
@@ -107,55 +111,98 @@ class Level1 extends React.Component {
       });
   }
 
+  handleRedirect() {
+    setTimeout(() => {
+      window.location.reload();
+    }, 10)
+  }
+
+  handleAudio(event) {
+    event.preventDefault();
+    var elm = event.target;
+    var audio = document.getElementById('audio');
+    var source = document.getElementById('audioSource');
+    source.setAttribute('src', elm.getAttribute('data-value'));
+    audio.load();
+    audio.play();
+  }
+
   render() {
     return (
       <div style={{ padding: '1rem', border: '1px solid grey', borderRadius: '4px', maxWidth: 400, margin: '3rem, auto' }} >
-        <h1>Vocabulary Quiz</h1>
-        <Progress percent={0} status='active' />
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <h2>LEVEL 1</h2>
-          <h2>1/5</h2>
-        </div>
-        <span style={{ marginBottom: 0, color: 'grey' }}>Infinitive</span>
-        <h2>Voca</h2>
-        <div style={{ fontSize: '1rem' }}>Answer the voca's <span style={{ color: 'red' }}>past participle</span></div>
-        <form style={{ padding: '1rem 0' }} onSubmit={this.handleSubmit}>
-          <div style={{ display: 'flex' }}>
-            <Input name='value' onChange={this.handleChange} id='voca' type="text" />
-          </div>
-          <Button className type='submit' onClick={this.handleSubmit}>Submit</Button>
-        </form>
 
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Button disabled={this.state.timer <= 8 ? true : false}>5</Button>
-          <Button disabled={this.state.timer <= 6 ? true : false}>4</Button>
-          <Button disabled={this.state.timer <= 4 ? true : false}>3</Button>
-          <Button disabled={this.state.timer <= 2 ? true : false}>2</Button>
-          <Button disabled={this.state.timer <= 0 ? true : false}>1</Button>
-          <Button onClick={this.handleRestart} style={{ display: this.state.timeOut ? 'block' : 'none' }}>Click to Restart!</Button>
-        </div>
+        {this.state.round < level1.length ?
 
-        <Divider />
+          <React.Fragment>
+            <h1>Vocabulary Quiz</h1>
+            <Progress percent={(this.state.round / level1.length) * 100} status='active' />
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <h2>LEVEL 1</h2>
+              <h2>{`${this.state.round} / ${level1.length}`}</h2>
+            </div>
+            <span style={{ marginBottom: 0, color: 'grey' }}>Infinitive</span>
+            <h2>{level1[this.state.round].voca}</h2>
+            <div style={{ fontSize: '1rem' }}>Answer the voca's <span style={{ color: 'red' }}>{this.state.randomTense === 'simple' ? 'simple past' : 'past participle'}</span></div>
+            <form style={{ padding: '1rem 0' }} onSubmit={this.handleSubmit}>
+              <div style={{ display: 'flex' }}>
+                <Input name='value' onChange={this.handleChange} id='voca' type="text" value={this.state.value} />
+              </div>
+              <Button className type='submit' onClick={this.handleSubmit}>Submit</Button>
+            </form>
 
-        <h3>Wrong! Correct answer: </h3>
-        <div>
-          <li style={{ display: 'block' }}>
-            <p>
-              icon answer
-            </p>
-          </li>
-        </div>
-        <h1>Review the wrong answers!</h1>
-        <div>
-          <ul>
-            <li>answer</li>
-            <li>answer</li>
-          </ul>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-          <Button>Retry</Button>
-          <Button>Level 2</Button>
-        </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button disabled={this.state.timer <= 8 ? true : false}>5</Button>
+              <Button disabled={this.state.timer <= 6 ? true : false}>4</Button>
+              <Button disabled={this.state.timer <= 4 ? true : false}>3</Button>
+              <Button disabled={this.state.timer <= 2 ? true : false}>2</Button>
+              <Button disabled={this.state.timer <= 0 ? true : false}>1</Button>
+              <Button onClick={this.handleRestart} style={{ display: this.state.timeOut ? 'block' : 'none' }}>Click to Restart!</Button>
+            </div>
+
+
+            {this.state.wrongAnswer &&
+              <React.Fragment>
+                <Divider />
+                <h3>Wrong! Correct answer: </h3>
+                <div>
+                  <li style={{ display: 'block' }}>
+                    <p onClick={this.handleAudio} data-value={level1[this.state.round - 1] ? level1[this.state.round - 1].mp3 : level1[this.state.round].mp3}>
+                      <AudioFilled />
+                      {this.state.wrongAnswer}
+                    </p>
+                  </li>
+                  <audio id='audio' controls style={{ display: 'none' }}>
+                    <source src='' id='audioSource'></source>
+                    Your browser doen't support the audio format
+                  </audio>
+                </div>
+              </React.Fragment>
+            }
+          </React.Fragment>
+
+          :
+
+          <React.Fragment>
+            <h1>Review the wrong answers!</h1>
+            <div>
+              <ul>
+                {this.state.wrongAnswers.map((answer, index) => {
+                  return <li key='index'>{answer}</li>
+                })}
+              </ul>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+              <Button onClick={this.handleRedirect}>Retry</Button>
+              <Button><Link to='/level2'></Link>Level 2</Button>
+            </div>
+          </React.Fragment>
+
+
+        }
+
+
+
+
       </div >
     );
   }
